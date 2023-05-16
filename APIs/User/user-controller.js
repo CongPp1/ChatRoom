bf8baef2423b07f96165db92ea85e2502f95c5ed
CongPp1 <thandiacung@gmail.com>
2023-05-16 14:52:30 +0700

    EditSocket

--- APIs/User/user-controller.js
+++ APIs/User/user-controller.js
@@ -1,4 +1,6 @@
 const userServices = require('../User/user-service')
+const model = require('../../models/index')
+
 
 const getAllUsers = async (req, res, next) => {
     try {
@@ -23,20 +25,20 @@ const getUsersByChatId = async (req, res, next) => {
     }
 }
 
-const createOrUpdateNickname = async (req, res, next) => {
-    try {
-        const { nickname } = req.body;
-        if (!nickname) {
-            return res.status(400).send({ message: 'Nickname is required' });
-        }
-        const { id } = req.params;
-        await userServices.createOrUpdateNickname(parseInt(id), req.body);
-        return res.status(200).send({ message: 'Success' })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({ message: 'Failed to update nickname', error: error.message })
-    }
-}
+// const createOrUpdateNickname = async (req, res, next) => {
+//     try {
+//         const { nickname } = req.body;
+//         if (!nickname) {
+//             return res.status(400).send({ message: 'Nickname is required' });
+//         }
+//         const { id } = req.params;
+//         await userServices.createOrUpdateNickname(parseInt(id), req.body);
+//         return res.status(200).send({ message: 'Success' })
+//     } catch (error) {
+//         console.log(error)
+//         return res.status(500).send({ message: 'Failed to update nickname', error: error.message })
+//     }
+// }
 
 const saveGeoLocation = async (req, res, next) => {
     try {
@@ -56,13 +58,18 @@ const saveGeoLocation = async (req, res, next) => {
     }
 }
 
-const out = async (req, res, next) => {
+const socketOut = async (req, res, next) => {
     try {
-        const { userId } = req.body;
-        if (!userId) {
-            return res.status(404).send({ message: 'UserID is required' })
+        const { socketIdOut } = req.body;
+        const user = await model.User.findOne(
+            { where: { socketId: socketIdOut } }
+        )
+        if (user.socketId !== null && socketIdOut !== null) {
+            await model.User.update(
+                { socketId: null, isActive: false },
+                { where: { socketId: socketIdOut } }
+            )
         }
-        await userServices.out(req.body);
         return res.status(200).send({ message: 'Success' });
     } catch (error) {
         console.log(error)
@@ -70,4 +77,22 @@ const out = async (req, res, next) => {
     }
 }
 
-module.exports = { getAllUsers, createOrUpdateNickname, getUsersByChatId, saveGeoLocation, out };
+
+const socketIn = async (req, res, next) => {
+    try {
+        const { socketIdIn } = req.body;
+        if (socketIdIn !== null) {
+            await model.User.update({
+                socketId: socketIdIn
+            }, {
+                where: { id: Number(req.params.id) },
+            });
+        }
+        return res.status(200).send({ message: 'Success' });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({ message: 'Error', error: error.message })
+    }
+}
+
+module.exports = { getAllUsers, getUsersByChatId, saveGeoLocation, socketOut, socketIn };
--- APIs/User/user-route.js
+++ APIs/User/user-route.js
@@ -4,8 +4,9 @@ const userController = require('./user-controller');
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUsersByChatId);
-router.put('/:id', userController.createOrUpdateNickname);
+// router.put('/:id', userController.createOrUpdateNickname);
 router.put('/geoLocation/:id', userController.saveGeoLocation);
-router.put('/out', userController.out);
+router.put('/socketIn/:id', userController.socketIn);
+router.put('/socketOut', userController.socketOut);
 
 module.exports = router;
